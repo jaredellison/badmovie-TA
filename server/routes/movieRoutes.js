fc495127e8012c7473841d431caba0d2d2e716af
Jared Ellison <jaredellison@gmail.com>
2018-12-10 00:41:47 -0500

    Add server route for getting favorite movies

--- server/routes/movieRoutes.js
+++ server/routes/movieRoutes.js
@@ -4,6 +4,7 @@ const movieController = require('../controllers/movieController.js');
 //Route different requests to different endpoints
 router.get('/search', movieController.getSearch)
 router.get('/genres', movieController.getGenres)
+router.get('/favorites', movieController.getFaves)
 router.post('/save', movieController.saveMovie)
 router.delete('/delete', movieController.deleteMovie)
 
